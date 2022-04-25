54248fcaa5dc77a89cf44b4fba5dfbb4ca866375
joranegs92 <joranegs92@gmail.com>
2022-04-25 23:24:35 +0900

    카카오

--- src/kakak2020.java
+++ src/kakak2020.java
@@ -3,11 +3,33 @@ public class kakak2020 {
         public static String solution(int[] numbers, String hand) {
             String answer = "";
             StringBuilder sb = new StringBuilder();
+            int right = 0;
+            int left = 0;
+
             for(int number : numbers){
                 if(number%3 == 1){
                     sb.append("L");
+                    left = number;
                 }else if(number%3 == 0){
                     sb.append("R");
+                    right =number;
+                }else{
+                    if(Math.abs(number-left) > Math.abs(number-right)){
+                        sb.append("R");
+                        right =number;
+                    }else if(Math.abs(number-left) < Math.abs(number-right)){
+                        sb.append("L");
+                        left = number;
+
+                    }else {
+                        if(hand =="right"){
+                            sb.append("R");
+                            right =number;
+                        }else {
+                            sb.append("L");
+                            left = number;
+                        }
+                    }
                 }
             }
             System.out.println(sb);
@@ -16,6 +38,7 @@ public class kakak2020 {
      public static void main(String[] args){
             int[] num = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
             String hand = "right";
+            solution(num,hand);
 
      }
 
