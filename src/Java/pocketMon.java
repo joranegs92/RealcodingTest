92d1dd42eb6a5716d18834326f12fafd1f864bb3
joranegs92 <joranegs92@gmail.com>
2022-11-14 22:42:29 +0900

    해쉬

---
+++ src/Java/pocketMon.java
@@ -0,0 +1,28 @@
+package Java;
+
+import java.util.HashSet;
+
+public class pocketMon {
+	public static int solution(int[] nums){
+		int answer = 0;
+		int max = nums.length/2;
+
+		HashSet<Integer> hashSet = new HashSet<>();
+
+		for(int num : nums){
+			hashSet.add(num);
+		}
+
+		if(hashSet.size() > max ){
+			answer = max;
+		}else{
+			answer = hashSet.size();
+		}
+		System.out.println(answer);
+		return answer;
+	}
+	public static void main(String[] args){
+		int[] nums = {3,3,3,2,2,2};
+		solution(nums);
+	}
+}
