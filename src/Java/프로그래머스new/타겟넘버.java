e47c66e81a87054420b8c0ee3b4b7c0f69cbd263
joranegs92 <joranegs92@gmail.com>
2023-11-15 20:57:40 +0900

    알고리즘!

--- src/Java/프로그래머스new/타겟넘버.java
+++ src/Java/프로그래머스new/타겟넘버.java
@@ -1,18 +1,37 @@
 package Java.프로그래머스new;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class 타겟넘버 {
 	static boolean[] d;
 	static int count;
+	static boolean[][] visited;
+	static int[][] A; // 데이터저장 2차원배열
+	static int N, M;//좌표 끝
+	static List<Integer>[] B;
 	public static void main(String[] args) {
+		int num[] = {1,1,1,1,1};
+		int answer = 0;
+		B = new ArrayList[num.length];
+		for(int i=0;i <num.length;i++){
+			B[i] = new ArrayList<>();
+			B[i].add(-1);
+			B[i].add(+1);
 
+		}
 
 
 	}
-	public static int DFS(){
 
-		int co = 0;
 
-		return co;
+	public static void BFS(int i, int j) {
+
+		/*
+		* 어떻게  + -를 만들지 않고...
+		*
+		* */
 	}
 
 
