a9e26d1203ceb45e63d4bdc3cb3fe2d8bcd2b48b
joranegs92 <joranegs92@gmail.com>
2023-12-07 14:15:19 +0900

    알고리즘!

--- src/Java/프로그래머스new/소수찾기.java
+++ src/Java/프로그래머스new/소수찾기.java
@@ -9,6 +9,12 @@ public class 소수찾기 {
 		int answer = 0;
 		Set<Integer> hashSet = new HashSet<>();
 		test("",numbers,hashSet);
+		int count = 0;
+		for(int num : hashSet){
+			if(check(num)){
+				count++;
+			}
+		}
 		/*
 		* 소수 구하기 = 2 랑 3만 나눠보기
 		* */
@@ -17,20 +23,29 @@ public class 소수찾기 {
 			String num = String.valueOf(numbers.charAt(i));
 			System.out.println(num);
 		}*/
-
+		System.out.println(count);
 		return answer;
 	}
-	public static void test(String test,String num,Set<String> set){
+	public static boolean check(int num ){
+		if(num <=1){
+			return false;
+		}
+		for(int i=2;i*i<=num; i++){
+			if(num % i ==0) return false;
+		}
+		return true;
+	}
+	public static void test(String test,String num,Set<Integer> set){
+		if(!test.isEmpty()){
+			set.add(Integer.parseInt(test));
+		}
 		for(int i = 0;i<num.length();i++){
-			String newTest = test+ num.charAt(i);
-			String newTest2 = num.substring(0,i)+num.substring(i+1);
-			System.out.println(newTest);
-			System.out.println(newTest2);
-			test(newTest,newTest2,test);
+
+			test(test+num.charAt(i),num.substring(0,i)+num.substring(i+1),set);
 		}
 	}
 	public static void main(String[] args) {
-		String num = "173";
+		String num = "17";
 		solution(num);
 	}
 }
