3c054f8791be0bf059bbbcbd89982273baf343e9
joranegs92 <joranegs92@gmail.com>
2023-12-18 22:07:08 +0900

    알고리즘!

--- src/Java/프로그래머스new/조이스틱.java
+++ src/Java/프로그래머스new/조이스틱.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class 조이스틱 {
 	public static void main(String[] args) {
 
-		solution("JEROEN");
+		solution("AAAAAA");
 	}
 	public static int solution(String name) {
 		String alpabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -18,15 +18,31 @@ public class 조이스틱 {
 		}
 
 		int beforeNum = 0;
-		for(String a :al){
-			int targetNum = alpabet.indexOf(a);
+		int move = name.length()-1;
 
-			//System.out.println(Math.min(targetNum - alpabet.indexOf("A"),alpabet.indexOf("Z")-targetNum+1));
+		for(int i =0; i<name.length();i++){
+			//int targetNum = alpabet.indexOf(name.charAt(i));
+			answer += Math.min(name.charAt(i) - 'A','Z'-name.charAt(i)+1);
 
-			System.out.println(alpabet.indexOf("Z")-targetNum+1);
+
+
+			int next = i+1;
+			while (next< name.length() && name.charAt(next)=='A'){
+				next++;
+			}
+
+
+
+			move = Math.min(move,i+name.length() - next + Math.min(i,name.length() - next));
+			System.out.println(move);
+		//	System.out.println(Math.min(targetNum - alpabet.indexOf("A"),alpabet.indexOf("Z")-targetNum+1));
+
+			//System.out.println(alpabet.indexOf("Z")-targetNum+1);
 			//Math.min(alpabet.indexOf("A")targetNum)
 
 		}
+		answer += move;
+		System.out.println( answer);
 /*		while (al.isEmpty()){
 			//Z, A면 무조건 1만 이동함
 			//
