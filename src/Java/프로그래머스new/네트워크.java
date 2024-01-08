8535036f56a2e03712fefb12df820d2729b0a603
joranegs92 <joranegs92@gmail.com>
2024-01-08 23:33:56 +0900

    알고리즘!

---
+++ src/Java/프로그래머스new/네트워크.java
@@ -0,0 +1,24 @@
+package Java.프로그래머스new;
+
+import java.util.ArrayList;
+
+public class 네트워크 {
+	static ArrayList<Integer>[] A;
+	public static void main(String[] args) {
+		int[][] cp= {{1,1,0},{1,1,0},{0,0,1}};
+		solution(3,cp);
+	}
+	public static int solution(int n, int[][] computers) {
+		int answer = 0;
+		A = new ArrayList[n-1];
+		for (int i=0;i<computers.length;i++){
+			A[i] = new ArrayList<>();
+		}
+
+		for ()
+		return answer;
+	}
+	public  static void DFS(){
+
+	}
+}
