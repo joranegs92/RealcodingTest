b8baccfb09c3dc014e1abc6b71a39133811d0cec
joranegs92 <joranegs92@gmail.com>
2024-01-22 16:36:29 +0900

    알고리즘!

---
+++ src/Java/프로그래머스new/산책pcce.java
@@ -0,0 +1,32 @@
+package Java.프로그래머스new;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/250129
+public class 산책pcce {
+	public int[] solution(String route) {
+
+		int east = 0;
+		int north = 0;
+		int[] answer = new int [2];
+		for (int i =0; i< route.length();i++){
+			switch (route.charAt(i)){
+				case 'N':
+					north++;
+					break;
+				case 'S':
+					north--;
+					break;
+				case 'E':
+					east++;
+					break;
+				case 'W':
+					east--;
+					break;
+			}
+		}
+		answer[0] = east;
+		answer[1] = north;
+
+		return answer;
+
+	}
+}
