6884097fd4c7ca38a01c602eed7c849e4a93ee87
joranegs92 <joranegs92@gmail.com>
2024-01-26 18:12:04 +0900

    알고리즘!

---
+++ src/Java/프로그래머스new/이진변환하기.java
@@ -0,0 +1,42 @@
+package Java.프로그래머스new;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*https://school.programmers.co.kr/learn/courses/30/lessons/70129*/
+public class 이진변환하기 {
+	public static void main(String[] args) {
+		solution("110010101001");
+	}
+	public static int[] solution(String s) {
+		int[] answer = {};
+		String ss ="";
+		int countzero =0; //
+		int binaryConut = 0;
+		List<Integer> integerList = new ArrayList<>();
+		for (int i =0; i<s.length();i++){
+			integerList.add(Integer.parseInt(s.substring(i)));
+		}
+		while (true){
+			int insiceConut =0;
+			binaryConut++;
+			integerList.stream().forEach(i -> );
+			for (int i =0;i<s.length();i++){
+
+				if(s.charAt(i)== '1'){
+					ss+=s.charAt(i);
+
+				}else {
+					insiceConut++;
+
+				}
+			}
+			ss = Integer.toBinaryString(Integer.parseInt(ss));
+			if(insiceConut<1){
+				break;
+			}*/
+		}
+
+		return answer;
+	}
+}
