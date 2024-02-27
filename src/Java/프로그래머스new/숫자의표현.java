5d33a1cfa4bf89f6a80c6eb761ebfee32a344989
joranegs92 <joranegs92@gmail.com>
2024-02-27 23:34:28 +0900

    알고리즘!

---
+++ src/Java/프로그래머스new/숫자의표현.java
@@ -0,0 +1,15 @@
+package Java.프로그래머스new;
+
+/*https://school.programmers.co.kr/learn/courses/30/lessons/12924*/
+public class 숫자의표현 {
+	public static void main(String[] args) {
+		int n = 15;
+		solution(n);
+	}
+	public static int solution(int n){
+		int answer = 0;
+
+
+		return answer;
+	}
+}
---
+++ src/Java/프로그래머스new/이진변환반복하기.java
@@ -0,0 +1,14 @@
+package Java.프로그래머스new;
+
+/*https://school.programmers.co.kr/learn/courses/30/lessons/70129*/
+public class 이진변환반복하기 {
+	public static void main(String[] args) {
+		String s = "110010101001";
+		solution(s);
+	}
+	public static int[] solution(String s){
+		int[] answer = new int[2];
+
+		return answer;
+	}
+}
